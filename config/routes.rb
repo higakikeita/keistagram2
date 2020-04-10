b56ffffed377133f871368bf6ca16fa15cb977a6
@higakikeita <higaki.keichan@gmail.com>
2020-04-11 00:51:10 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,controllers: { registrations: 'registrations' }
   root 'pages#home'
   get '/users/:id', to: 'users#show', as: 'user'
-  resources :posts, only: %i(new create) do
+  resources :posts, only: %i(new create index) do
     resources :photos, only: %i(create)
   end
 end
