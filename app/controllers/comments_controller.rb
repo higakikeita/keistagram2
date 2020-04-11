9bf56b023a5e2da4f5816f637ab24e94041f4deb
@higakikeita <higaki.keichan@gmail.com>
2020-04-11 20:21:46 +0900

    destroyアクション編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,15 @@ class CommentsController < ApplicationController
       flash[:alert] = "コメントに失敗しました"
     end
   end
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @post = @comment.post
+    if @comment.destroy
+      respond_to :js
+    else
+      flash[:alert] = "コメントの削除に失敗しました"
+    end
+  end
   private
   def comment_params
     params.required(:comment).permit(:user_id, :post_id, :comment)
